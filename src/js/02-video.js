906df11f5683d555eed8bf350e12afb49ea2c60c
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-03-26 02:00:51 +0200

    app

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe')
-const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+const player = new Player('vimeo-player');
 
 player.on(
     "timeupdate",throttle(function(data){
-        localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+        localStorage.setItem(CURRENT_TIME, data.seconds);
     }, 1000)
 );
-const currentTime = localStorage.getItem("videoplayer-current-time");
-const time = JSON.parse(currentTime);
-player
-.setCurrentTime(time.seconds || 0)
-.then(function(seconds){})
-.catch(function(error){
-    switch(error.name){
-        case "RangeError":
-            break;
-        default:
-            break;
-    }
-})
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
